292be18d4e76b126b09816df9f63f93c3672611c
rkrasiuk <rokrassyuk@gmail.com>
2019-06-07 11:11:01 -0500

    frstio tsconfig

--- packages/graphql-playground-react/typings/styled-jsx.d.ts
+++ packages/graphql-playground-react/typings/styled-jsx.d.ts
@@ -1,15 +1,15 @@
 import 'react'
 
 declare module 'react' {
-  interface HTMLProps<T> {
-    jsx?: boolean
-    global?: boolean
-  }
+	interface HTMLProps<T> {
+		jsx?: boolean
+		global?: boolean
+	}
 }
 
 declare module 'react' {
-  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
-    jsx?: boolean
-    global?: boolean
-  }
+	interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+		jsx?: boolean
+		global?: boolean
+	}
 }
