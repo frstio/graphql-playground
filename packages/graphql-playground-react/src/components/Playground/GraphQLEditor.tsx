28aa7db920ee3e93e14cac07a6e6e3adbb5d1c90
rkrasiuk <rokrassyuk@gmail.com>
2019-06-10 13:23:16 -0500

    remove tracing window

--- packages/graphql-playground-react/src/components/Playground/GraphQLEditor.tsx
+++ packages/graphql-playground-react/src/components/Playground/GraphQLEditor.tsx
@@ -15,7 +15,6 @@ import {
 } from './VariableEditor'
 import Spinner from '../Spinner'
 import Results from './Results'
-import ResponseTracing from './ResponseTracing'
 import { fillLeafs } from 'graphiql/dist/utility/fillLeafs'
 import { getLeft, getTop } from 'graphiql/dist/utility/elementPosition'
 
@@ -250,22 +249,6 @@ class GraphQLEditor extends React.PureComponent<Props & ReduxProps> {
 							{this.props.subscriptionActive && (
 								<Listening>Listening &hellip;</Listening>
 							)}
-							<ResponseTracking
-								isOpen={this.props.responseTracingOpen}
-								height={this.props.responseTracingHeight}
-							>
-								<ResponseTrackingTitle
-									isOpen={this.props.responseTracingOpen}
-									onMouseDown={this.handleTracingResizeStart}
-								>
-									<VariableEditorSubtitle isOpen={false}>
-										Tracing
-									</VariableEditorSubtitle>
-								</ResponseTrackingTitle>
-								<ResponseTracing
-									open={this.props.responseTracingOpen}
-								/>
-							</ResponseTracking>
 						</ResultWrap>
 					</EditorBar>
 				</EditorWrapper>
@@ -474,45 +457,6 @@ class GraphQLEditor extends React.PureComponent<Props & ReduxProps> {
 			event.target === this.queryResizer ||
 			event.target === this.responseResizer
 		)
-	}
-
-	private handleTracingResizeStart = downEvent => {
-		downEvent.preventDefault()
-
-		let didMove = false
-		const hadHeight = this.props.responseTracingHeight
-		const offset = downEvent.clientY - getTop(downEvent.target)
-
-		let onMouseMove: any = moveEvent => {
-			if (moveEvent.buttons === 0) {
-				return onMouseUp()
-			}
-
-			didMove = true
-
-			const editorBar = ReactDOM.findDOMNode(this.editorBarComponent)
-			const topSize = moveEvent.clientY - getTop(editorBar) - offset
-			const bottomSize = editorBar.clientHeight - topSize
-			if (bottomSize < 60) {
-				this.props.closeTracing(hadHeight)
-			} else {
-				this.props.openTracing(hadHeight)
-			}
-		}
-
-		let onMouseUp: any = () => {
-			if (!didMove) {
-				this.props.toggleTracing()
-			}
-
-			document.removeEventListener('mousemove', onMouseMove)
-			document.removeEventListener('mouseup', onMouseUp)
-			onMouseMove = null
-			onMouseUp = null
-		}
-
-		document.addEventListener('mousemove', onMouseMove)
-		document.addEventListener('mouseup', onMouseUp)
 	}
 
 	private handleVariableResizeStart = downEvent => {
@@ -743,19 +687,6 @@ const VariableEditorSubtitle = styled.span<TitleProps>`
 	&:last-child {
 		margin-right: 0;
 	}
-`
-
-const ResponseTracking = styled(BottomDrawer)`
-	background: ${p => p.theme.editorColours.rightDrawerBackground};
-`
-
-const ResponseTrackingTitle = styled(({ isOpen, ...rest }) => (
-	<BottomDrawerTitle {...rest} />
-))`
-	text-align: right;
-	background: ${p => p.theme.editorColours.rightDrawerBackground};
-	cursor: ${props => (props.isOpen ? 's-resize' : 'n-resize')};
-	color: ${p => p.theme.editorColours.drawerTextInactive};
 `
 
 interface QueryProps {
